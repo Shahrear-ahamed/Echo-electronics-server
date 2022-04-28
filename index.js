8f3d49b6f4d89e3d54e8198ed073efa9d4655c12
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-04-28 22:39:22 +0600

    change file name

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
@@ -9,7 +10,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// add mongodb server
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uceta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 // server api making code are here
+
+
 
 // server start are here
 app.get("/", (req, res) => {
