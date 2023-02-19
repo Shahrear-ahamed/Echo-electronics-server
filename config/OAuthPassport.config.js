5b73830ef7c53a09e4e0d4c00fc3db6c8e9e41df
Shahrear-ahamed <its.shahrear@gmail.com>
2023-02-19 21:19:17 +0600

    oauth with google some change in nameing

--- config/OAuthPassport.config.js
+++ config/OAuthPassport.config.js
@@ -1,17 +1,24 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-const strategy = new GoogleStrategy(
+// google strategy configuration
+const googleStrategy = new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET_ID,
     callbackURL: "http://localhost:5000/api/v2/auth/google/redirect",
   },
   (accessToken, refreshToken, profile, cb) => {
-    const userDetails = { ...profile?._json };
+    const userDetails = {
+      id: profile?.id,
+      name: profile?.displayName,
+      provider: profile?.provider,
+      email: profile?.emails[0]?.value,
+      photo: profile?.photos[0]?.value,
+    };
 
     if (profile?.id) return cb(null, userDetails);
   }
 );
 
-passport.use(strategy);
+passport.use(googleStrategy);
--- routers/v2/OAuth.router.js
+++ routers/v2/OAuth.router.js
@@ -7,14 +7,18 @@ require("../../config/OAuthPassport.config");
 
 // user click and process start from here
 // http://localhost:5000/api/v2/auth/google
-router
-  .route("/google")
-  .get(passport.authenticate("google", { scope: ["profile", "email"] }));
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-// after google auth process done, user will be redirected to this route
+// after auth process done, user will be redirected to this route
 // http://localhost:5000/api/v2/auth/google/redirect
-router
-  .route("/google/redirect")
-  .get(passport.authenticate("google", { session: false }), authGoogleUser);
+router.get(
+  "/google/redirect",
+  passport.authenticate("google", { session: false }),
+  authGoogleUser
+);
+
 
 module.exports = router;
