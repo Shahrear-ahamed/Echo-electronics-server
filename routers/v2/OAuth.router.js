cb5d956a66a9abf50f6dc84490c0f5dcf2296a55
Shahrear-ahamed <its.shahrear@gmail.com>
2023-03-11 21:59:00 +0600

    auth login thing are change

--- routers/v2/OAuth.router.js
+++ routers/v2/OAuth.router.js
@@ -12,13 +12,38 @@ router.get(
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
+router.get("login/success", (req, res) => {
+  if (req.user) {
+    res.json({
+      success: true,
+      message: "user has successfully authenticated",
+      user: req.user,
+    });
+  }
+});
+
+router.get("login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "user failed to authenticate",
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout(); 
+  res.redirect(process.env.CLIENT_URL);
+});
+
 // after auth process done, user will be redirected to this route
 // http://localhost:5000/api/v2/auth/google/redirect
 router.get(
   "/google/redirect",
-  passport.authenticate("google", { session: false }),
+  passport.authenticate("google", {
+    session: false,
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login/failed",
+  }),
   authGoogleUser
 );
-
 
 module.exports = router;
